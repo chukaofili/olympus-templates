1999ae69a3dbc4bf442a34e4cedd2ab610b042cc
Chuka Ofili <ofili@microsmart.tk>
2017-10-12 14:23:44 +0100

    Initial Commit

---
+++ backend/config/settings.js
@@ -0,0 +1,9 @@
+const package = require('../package.json');
+
+module.exports.settings = {
+    api: {
+        title: 'Sample Backend Template',
+        name: 'sample-backend-api',
+        version: package.version
+    }
+}
